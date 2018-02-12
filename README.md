f1a2a6964bfc678ee8a4fc650ff6d8a65dcf4a5b
Eric Liao <rcliao01@gmail.com>
2018-02-12 07:39:19 -0800

    feat: add chunkwm instructions

--- README.md
+++ README.md
@@ -10,7 +10,12 @@ My personal dotfiles containing configurations for NeoVim, Tmux and Zshrc.
 * Install NeoVim – https://github.com/neovim/neovim/wiki/Installing-Neovim
     * Install Vim-Plug – https://github.com/junegunn/vim-plug
 * Install Oh My Zsh – https://github.com/robbyrussell/oh-my-zsh
+    * Install spaceship-primpt – https://github.com/denysdovhan/spaceship-prompt
 * Install Tmux – https://github.com/tmux/tmux
+
+### OSX WM
+
+* Install Chunkwm – https://github.com/koekeishiya/chunkwm
 
 ### Set up
 
--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ tmux_conf=~/dotfiles/.tmux/.tmux.conf
 tmux_conf_dest=~/.tmux.conf
 zshrc=~/dotfiles/.zsh/.zshrc
 zshrc_dest=~/.zshrc
+chunkwmrc=~/dotfiles/chunkwm/.chunkwmrc
+chunkwmrc_dest=~/.chunkwmrc
+skhdrc=~/dotfiles/chunkwm/.skhdrc
+skhdrc_dest=~/.skhdrc
 
 echo "Setting up $vimrc => $vimrc_dest"
 ln -sf $vimrc $vimrc_dest
@@ -13,3 +17,7 @@ echo "Setting up $tmux_conf => $tmux_conf_dest"
 ln -sf $tmux_conf $tmux_conf_dest
 echo "Setting up $zshrc => $zshrc_dest"
 ln -sf $zshrc $zshrc_dest
+echo "Setting up $chunkwmrc => $chunkwmrc_dest"
+ln -sf $chunkwmrc $chunkwmrc_dest
+echo "Setting up $skhdrc => $skhdrc_dest"
+ln -sf $skhdrc $skhdrc_dest
