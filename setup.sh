7ae5e2ff175af2a8d75f6c89d83a39529d3a0c49
Eric Liao <eric@edlio.com>
2018-03-12 11:39:07 -0700

    feat: add mpd/ncmpcpp configurations

--- setup.sh
+++ setup.sh
@@ -14,7 +14,12 @@ ranger_config=~/dotfiles/ranger
 ranger_config_dest=~/.config/ranger
 alacritty_config=~/dotfiles/alacritty/alacritty.yml
 alacritty_config_dest=~/.config/alacritty/alacritty.yml
+mpd_conf=~/dotfiles/mpd/mpd.conf
+mpd_conf_dest=~/.mpd/mpd.conf
+ncmpcpp_bindings=~/dotfiles/ncmpcpp/bindings
+ncmpcpp_bindings_dest=~/.ncmpcpp/bindings
 
+mkdir -p ~/.mpd && touch ~/.mpd/mpd{.db,.log,.pid,state}
 echo "Setting up $vimrc => $vimrc_dest"
 ln -sf $vimrc $vimrc_dest
 echo "Setting up $tmux_conf => $tmux_conf_dest"
@@ -29,3 +34,7 @@ echo "Setting up $ranger_config => $ranger_config_dest"
 ln -sf $ranger_config $ranger_config_dest
 echo "Setting up $alacritty_config => $alacritty_config_dest"
 ln -sf $alacritty_config $alacritty_config_dest
+echo "Setting up $mpd_conf => $mpd_conf_dest"
+ln -sf $mpd_conf $mpd_conf_dest
+echo "Setting up $ncmpcpp_bindings => $ncmpcpp_bindings_dest"
+ln -sf $ncmpcpp_bindings $ncmpcpp_bindings_dest
